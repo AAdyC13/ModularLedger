b0cbdf2b3861581b030b2a4c5d269e134701da66
AAdyC13 <aadycheng@gmail.com>
2025-11-11 12:54:51 +0800

    新 Hybrid App 架構

--- android/app/src/main/kotlin/com/github/AAdyC13/modular_ledger/modular_ledger/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.github.AAdyC13.modular_ledger.modular_ledger
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ app/src/main/java/com/example/modular_ledger/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.modular_ledger
+
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.modular_ledger.ui.theme.Modular_ledgerTheme
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url ?: "")
+                return true
+            }
+        }
+
+        WebView.setWebContentsDebuggingEnabled(true)
+
+        // 若是本地前端 (放在 assets/www)
+        //webView.loadUrl("file:///android_asset/www/index.html")
+
+        // 測試網頁
+        webView.loadUrl("https://www.example.com/")
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Modular_ledgerTheme {
+        Greeting("Android")
+    }
+}
